61ed0f8dbb389c36f8014757a1aae1b39b9f7000
Ajitesh Kumar <ajiteshkmr802@gmail.com>
2021-05-01 21:28:28 +0530

    Create MiniUber.java
    Description
    Support two basic uber features:
    
    Drivers report their locations.
    Rider request a uber, return a matched driver.
    When rider request a uber, match a closest available driver with him, then mark the driver not available.
    
    When next time this matched driver report his location, return with the rider's information.
    
    You can implement it with the following instructions:
    
    report(driver_id, lat, lng)
    If a matching trip is not found, return null.
    return matched trip information.
    request(rider_id, lat, lng)
    create a trip with rider's information.
    find a closest driver, mark this driver not available.
    fill driver_id into this trip.
    return trip
    This is the definition of Trip in Java:
    
    public class Trip {
        public int id; // trip's id, primary key
        public int driver_id, rider_id; // foreign key
        public double lat, lng; // pick up location
    }
    Example
    Example 1:
    
    Input:
      report(1, 36.1344, 77.5672) 
      report(2, 45.1344, 76.5672) 
      request(2, 39.1344, 76.5672) 
      report(1, 38.1344, 75.5672) 
      report(2, 45.1344, 76.5672) 
    Output:
      null
      null
      Trip(rider_id: 2, driver_id: 1, lat: 39.1344, lng: 76.5672)
      Trip(rider_id: 2, driver_id: 1, lat: 39.1344, lng: 76.5672)
      null
    Example 2:
    
    Input:
      report(1, 36.1344, 77.5672)
      report(2, 45.1344, 76.5672)
      request(2, 39.1344, 76.5672)
      request(3, 78.1344, 134.5672)
    Output:
      null
      null
      LOG(INFO): Trip(rider_id: 2, driver_id: 1, lat: 39.1344, lng: 76.5672)
      LOG(INFO): Trip(rider_id: 3, driver_id: 2, lat: 78.1344, lng: 134.5672)

---
+++ MiniUber.java
@@ -0,0 +1,76 @@
+/**
+ * Definition of Trip:
+ * public class Trip {
+ *     public int id; // trip's id, primary key
+ *     public int driver_id, rider_id; // foreign key
+ *     public double lat, lng; // pick up location
+ *     public Trip(int rider_id, double lat, double lng);
+ * }
+ * Definition of Helper
+ * class Helper {
+ *     public static double get_distance(double lat1, double lng1,
+                                         double lat2, double lng2) {
+ *         // return distance between (lat1, lng1) and (lat2, lng2)
+ *     }
+ * };
+ */
+class  Location {
+    double latitude;
+    double longitude;
+    public Location(double _latitude , double _longitude){
+        latitude = _latitude;
+        longitude = _longitude;
+    }    
+}
+
+public class MiniUber {
+    
+    // stores all Trip in progress cabs rider ids(key) and Trip Information(value). 
+    private Map<Integer,Trip> cabsTripInProgress; 
+    // stores all available cab's rider ids(key) and location deatail(value)
+    private Map<Integer,Location> availableCabsLocation; 
+
+    public MiniUber() {
+        cabsTripInProgress = new HashMap<>();
+        availableCabsLocation = new HashMap<>();
+    }
+    // @param driver_id an integer
+    // @param lat, lng driver's location
+    // return matched trip information if there have matched rider or null
+    public Trip report(int driver_id, double lat, double lng) {
+
+        if(cabsTripInProgress.containsKey(driver_id)){
+            return cabsTripInProgress.get(driver_id);
+        }
+        Location location = new Location(lat,lng);
+        availableCabsLocation.put(driver_id , location);
+        return null;
+        
+    }
+    // @param rider_id an integer
+    // @param lat, lng rider's location
+    // return a trip
+    public Trip request(int rider_id, double lat, double lng) {
+
+        Trip trip = new Trip(rider_id, lat , lng);
+        int matchedDriver = -1;
+        double minDistance = Integer.MAX_VALUE;
+        for(Integer driver_id : availableCabsLocation.keySet()){
+            Location location = availableCabsLocation.get(driver_id);
+            double distance = Helper.get_distance(lat,lng,location.latitude,location.longitude);
+            if(distance < minDistance){
+                minDistance = distance;
+                matchedDriver = driver_id;
+            }
+        }
+
+        if(matchedDriver!=-1){
+            availableCabsLocation.remove(matchedDriver);
+        }
+
+        trip.driver_id = matchedDriver;
+        cabsTripInProgress.put(matchedDriver,trip);
+
+        return trip;
+    }
+}
